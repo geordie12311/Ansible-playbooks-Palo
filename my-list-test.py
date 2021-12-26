f485d917208806cad005accd17201f36bddeaa55
Geordie12311 <80632286+geordie12311@users.noreply.github.com>
2021-12-26 15:32:40 +0000

    Add files via upload

---
+++ my-list-test.py
@@ -0,0 +1,11 @@
+my_number_of_paths = [1, 1, 1, 1, 1 2, 1, 1, 4, 2]
+
+my_multipath_list = []
+for paths in my_number_of_paths:
+    if paths > 1:
+        my_multipath_list append(paths)
+        
+if my multpath_list:
+    print(my_multipath_list)
+else:
+    print("There were no multipath values")
